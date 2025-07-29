15da6f58a8bc79eff88cddf7449a2039329f05f5
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-29 00:25:52 -0300

    adiciona o enunciado do exercício áo programa principal

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,13 +7,26 @@ import entities.Conta;
 
 public class Main {
 
+	/*
+	 * Em um bar, o ingresso custa 10 reais para homens e 8 reais para mulheres.
+	 * Cada cerveja custa 5 reais, cada refrigerante 3 reais e cada espetinho custa
+	 * 7. Além disso, o bar cobra uma taxa de couvert artístico no valor de 4 reais,
+	 * porém, se o valor gasto com consumo for superior a 30 reais, o couvert
+	 * artístico não é cobrado. Fazer um programa para ler os seguintes dados de um
+	 * cliente do bar: sexo (F ou M), quantidade de cervejas, refrigerantes e
+	 * espetinhos consumidos. O programa deve então mostrar um relatório com a conta
+	 * a ser paga pelo cliente. 
+	 */
+
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
+		Locale.setDefault(Locale.US); // Define o padrão de formatação numérica para o formato dos EUA (ex: ponto em
+										// vez de vírgula)
 		Scanner sc = new Scanner(System.in);
 
 		Conta conta = new Conta();
 
+		// entrada de dados do usuário para a impressão do relatório(conta)
 		System.out.print("Genero: ");
 		conta.genero = Character.toUpperCase(sc.next().charAt(0));
 		System.out.print("Quantidade de cervejas: ");
@@ -25,7 +38,8 @@ public class Main {
 
 		System.out.println();
 		System.out.println("RELATÓRIO:");
-		System.out.println("----------");
+		System.out.println("----------------------------------");
+		System.out.println("----------------------------------");
 
 		System.out.printf("Consumo: R$ %.2f%n", conta.consumo());
 
@@ -34,11 +48,9 @@ public class Main {
 		} else {
 			System.out.printf("Couvert: R$ %.2f%n", conta.couvert());
 		}
-		
+
 		System.out.printf("Ingresso: R$ %.2f%n", conta.ingresso());
-
 		System.out.println();
-
 		System.out.printf("Valor Total a Pagar: R$ %.2f%n", conta.valorTotal());
 
 		sc.close();
