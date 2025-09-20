0ffbcc6d4dd641092215867894cbac006070139e
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-09-19 23:09:34 -0300

    Identa o código Main.java e adiciona comentários ao código

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	 * artístico não é cobrado. Fazer um programa para ler os seguintes dados de um
 	 * cliente do bar: sexo (F ou M), quantidade de cervejas, refrigerantes e
 	 * espetinhos consumidos. O programa deve então mostrar um relatório com a conta
-	 * a ser paga pelo cliente. 
+	 * a ser paga pelo cliente.
 	 */
 
 	public static void main(String[] args) {
@@ -24,24 +24,23 @@ public class Main {
 										// vez de vírgula)
 		Scanner sc = new Scanner(System.in);
 
-		
-
 		// entrada de dados do usuário para a impressão do relatório(conta)
 		System.out.print("Genero: ");
-		char genero = Character.toUpperCase(sc.next().charAt(0)); // Converte para maiúsculo para evitar erro em 'f' ou 'm'
+		char genero = Character.toUpperCase(sc.next().charAt(0)); // Converte para maiúsculo para evitar erro em 'f' ou
+																	// 'm'
 		System.out.print("Quantidade de cervejas: ");
 		int cervejas = sc.nextInt();
 		System.out.print("Quantidade de refrigerantes: ");
 		int refrigerantes = sc.nextInt();
 		System.out.print("Quantidade de espetinhos: ");
 		int espetinhos = sc.nextInt();
-		// cria o objeto Conta que armazena os dados do cliente acessando os atributos da classe Conta
+		// cria o objeto Conta que armazena os dados do cliente acessando os atributos
+		// da classe Conta
 		Conta conta = new Conta(genero, cervejas, refrigerantes, espetinhos);
 
 		// geração do relatório
 		System.out.println();
 		System.out.println("RELATÓRIO:");
-		System.out.println("----------------------------------");
 		System.out.println("----------------------------------");
 
 		// exibe o valor do consumo total
@@ -56,10 +55,10 @@ public class Main {
 
 		// exibe o valor do ingresso (diferente para homem e mulher)
 		System.out.printf("Ingresso: R$ %.2f%n", conta.ingresso());
-		
+
 		System.out.println();
-		
-		//exibe o valor final da conta
+
+		// exibe o valor final da conta
 		System.out.printf("Valor Total a Pagar: R$ %.2f%n", conta.valorTotal());
 
 		sc.close();
